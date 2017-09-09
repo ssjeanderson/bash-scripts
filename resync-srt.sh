81b03e93d44ff66e2a4a7eb8be6fa602c9048229
ssjeanderson <ssjeanderson@gmail.com>
2017-09-08 22:08:14 -0300

    Update resync-srt.sh

--- resync-srt.sh
+++ resync-srt.sh
@@ -19,59 +19,61 @@ do
 
         start_milisec=${line:9:3}
         stop_milisec=${line:26:3}
-        
+
         new_start_hour=$start_hour
         new_stop_hour=$stop_hour
         new_start_min=$start_min
         new_stop_min=$stop_min
-        new_start_sec=$start_sec
-        new_stop_sec=$stop_sec
 
         new_start_sec=$(( $start_sec $operation $adjust_sec ))
-        if [[ $new_start_sec > 59 ]]; then
+        if [[ $new_start_sec -gt 59 ]]; then
             new_start_sec=$(( $start_sec + $adjust_sec - 60 ))
             new_start_min=$(( $start_min + 1))
-            if [[ $new_start_min > 59 ]]; then
+            if [[ $new_start_min -gt 59 ]]; then
                 new_start_min=0
                 new_start_hour=$(( $start_hour + 1 ))
             fi
         fi
 
-        if [[ $new_start_sec < 0 ]]; then
+        if [[ $new_start_sec -lt 0 ]]; then
             new_start_sec=$(( 60 + $start_sec - $adjust_sec ))
             new_start_min=$(($start_min - 1))
-            if [[ $new_start_min < 0 ]]; then
+            if [[ $new_start_min -lt 0 ]]; then
                 new_start_min=59
                 new_start_hour=$(( $start_hour - 1 ))
-                if [[ $new_start_min < 0 ]]; then
+                if [[ $new_start_hour -lt 0 ]]; then
                     new_start_hour=0
+                    new_start_min=0
+                    new_start_sec=0
                 fi
             fi
         fi
 
         new_stop_sec=$(( $stop_sec $operation $adjust_sec ))
-        if [[ $new_stop_sec > 59 ]]; then
+        if [[ $new_stop_sec -gt 59 ]]; then
             new_stop_sec=$(( $stop_sec + $adjust_sec - 60 ))
             new_stop_min=$(( $stop_min + 1))
-            if [[ $new_stop_min > 59 ]]; then
+            if [[ $new_stop_min -gt 59 ]]; then
                 new_stop_min=0
                 new_stop_hour=$(( $stop_hour + 1 ))
             fi
         fi
 
-        if [[ $new_stop_sec < 0 ]]; then
+        if [[ $new_stop_sec -lt 0 ]]; then
             new_stop_sec=$(( 60 + $stop_sec - $adjust_sec ))
             new_stop_min=$(($stop_min - 1))
-            if [[ $new_stop_min < 0 ]]; then
+            if [[ $new_stop_min -lt 0 ]]; then
                 new_stop_min=59
                 new_stop_hour=$(( $stop_hour - 1 ))
-                if [[ $new_stop_min < 0 ]]; then
+                if [[ $new_stop_hour -lt 0 ]]; then
                     new_stop_hour=0
+                    new_stop_min=0
+                    new_stop_sec=0
                 fi
             fi
         fi
 
-        echo "$new_start_hour:$new_start_min:$new_start_min,$start_milisec --> $new_stop_hour:$new_stop_min:$new_stop_min,$stop_milisec" >> $newfile
+        echo "$new_start_hour:$new_start_min:$new_start_sec,$start_milisec --> $new_stop_hour:$new_stop_min:$new_stop_sec,$stop_milisec" >> $newfile
     else
         echo "$line" >> $newfile
     fi
