4a97bcf0aa6195ceca76e573985bd51f4697a252
Jeanderson Silva <ssjeanderson@gmail.com>
2017-09-13 00:02:09 -0300

    revisao

--- resync-srt.sh
+++ resync-srt.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 timeshift="$1"
-filename="$2"
-newfile="$3"
+input_sub="$2"
+output_sub="$3"
 
 operation="${timeshift:0:1}"
-adjust_sec="${timeshift:1}"
+adjust_sec=$((10#${timeshift:1}))
+
+echo "" > $output_sub
 
 while read -r line
 do
     if [[ "$line" =~ ^[0-9][0-9]\: ]]; then
-        start_hour=${line:0:2}
-        start_min=${line:3:2}
-        start_sec=${line:6:2}
-        stop_hour=${line:17:2}
-        stop_min=${line:20:2}     
-        stop_sec=${line:23:2}
-
-        start_millisec=${line:9:3}
-        stop_millisec=${line:26:3}
+        start_hour=$((10#${line:0:2}))
+        start_min=$((10#${line:3:2}))
+        start_sec=$((10#${line:6:2}))
+        start_millisec=$((10#${line:9:3}))
+        stop_hour=$((10#${line:17:2}))
+        stop_min=$((10#${line:20:2}))     
+        stop_sec=$((10#${line:23:2}))
+        stop_millisec=$((10#${line:26:3}))
 
         new_start_hour=$start_hour
         new_start_min=$start_min
@@ -37,7 +38,7 @@ do
 
         if [[ $new_start_sec -lt 0 ]]; then
             new_start_sec=$(( 60 + $start_sec - $adjust_sec ))
-            new_start_min=$(($start_min - 1))
+            new_start_min=$(( $start_min - 1))
             if [[ $new_start_min -lt 0 ]]; then
                 new_start_min=59
                 new_start_hour=$(( $start_hour - 1 ))
@@ -61,7 +62,7 @@ do
 
         if [[ $new_stop_sec -lt 0 ]]; then
             new_stop_sec=$(( 60 + $stop_sec - $adjust_sec ))
-            new_stop_min=$(($stop_min - 1))
+            new_stop_min=$(( $stop_min - 1))
             if [[ $new_stop_min -lt 0 ]]; then
                 new_stop_min=59
                 new_stop_hour=$(( $stop_hour - 1 ))
@@ -73,8 +74,8 @@ do
             fi
         fi
 
-        echo "$new_start_hour:$new_start_min:$new_start_sec,$start_millisec --> $new_stop_hour:$new_stop_min:$new_stop_sec,$stop_millisec" >> $newfile
+        printf "%02d:%02d:%02d,%03d --> %02d:%02d:%02d,%03d\n" "$new_start_hour" "$new_start_min" "$new_start_sec" "$start_millisec" "$new_stop_hour" "$new_stop_min" "$new_stop_sec" "$stop_millisec" >> $output_sub
     else
-        echo "$line" >> $newfile
+        echo "$line" >> $output_sub
     fi
-done < "$filename"
+done < "$input_sub"
