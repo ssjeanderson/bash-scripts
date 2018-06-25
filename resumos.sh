20b504bd1adea0aff4ed914da3262a050304e80d
ssjeanderson <ssjeanderson@gmail.com>
2018-06-25 17:53:49 -0300

    Update resumos.sh

--- resumos.sh
+++ resumos.sh
@@ -8,11 +8,13 @@ for i in $lista; do
 	echo "dn: $i"
 done
 
-
 ####
+
 # Comenta todas as linhas de um arquivo
 sed -i.old -e 's/^/#/' arquivo
 
-
 # executar como expect do PAC. seta o color do vim
 alias vim='vim -c "color desert"'; history -d $((HISTCMD-1))
+
+# Captura saida do tcpdump em tempo real
+tcpdump -n --immediate-mode -l | grep stuff
