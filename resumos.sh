dd8033ed4cc87811fde715d437d7db3b0ac453c7
ssjeanderson <ssjeanderson@gmail.com>
2019-04-26 15:30:18 -0300

    Update resumos.sh

--- resumos.sh
+++ resumos.sh
@@ -8,6 +8,14 @@ for i in $lista; do
 	echo "dn: $i"
 done
 
+### While percorrer arquivo
+input="/path/to/txt/file"
+while IFS= read -r var
+do
+  echo "$var"
+done < "$input"
+
+
 ####
 
 # Comenta todas as linhas de um arquivo
@@ -18,3 +26,4 @@ alias vim='vim -c "color desert"'; history -d $((HISTCMD-1))
 
 # Captura saida do tcpdump em tempo real
 tcpdump -n --immediate-mode -l | grep stuff
+
